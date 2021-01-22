bb0a89ea764e013555c02c1b743a4d85f3b0ae75
Daniel Marlow <drm3107@gmail.com>
2021-01-22 17:35:40 +0000

    Tests for page class

--- PageTest.java
+++ PageTest.java
@@ -1,4 +1,6 @@
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -13,6 +15,17 @@ public void setUP(){
 @Test
 public void shouldReturnTheNumberOfPhotosInAPage(){
     assertEquals(10, page.getNumberOfPhotosInPage());
+}
+
+@Test
+public void shouldGetPhotoOnPage(){
+    assertNotNull(page.getPhoto(0));
+}
+
+@Test
+public void shouldBeAbleToAddPhotoToPage(){
+    page.addPhoto(new Photo());
+    assertEquals(11, page.getNumberOfPhotosInPage());
 }
 
 }
