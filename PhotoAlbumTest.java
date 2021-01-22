9e34adff9f83e6846d9789b2608295339bc4ef62
Daniel Marlow <drm3107@gmail.com>
2021-01-22 17:59:26 +0000

    Refactord test

--- PhotoAlbumTest.java
+++ PhotoAlbumTest.java
@@ -10,10 +10,14 @@ import org.junit.Before;
 public class PhotoAlbumTest {
     
    PhotoAlbum photoAlbum;
+   Page page0;
+   Page page1;
 
     @Before
 	public void setUP() {
         photoAlbum = new PhotoAlbum(10);
+        page0 = new Page(1);
+        page1 = new Page(0);
     }
     
     @Test
@@ -33,13 +37,18 @@ public class PhotoAlbumTest {
         assertEquals(11, photoAlbum.getNumberOfPagesInAlbum());
     }
 
+
+    // This is the BIG end to end test describing what we are trying to achieve
     @Test
     public void shouldBeAbleToMovePhotoFromOnePageToAnother(){
 
-        Page page0 = new Page(1);
-        Page page1 = new Page(0);
+        // Arrange the test with test data
         photoAlbum = new PhotoAlbum(Arrays.asList(page0, page1));
+
+        // Act- perform the operation under test
         photoAlbum.movePhotoBetweenPages(0, 0, 1);
+
+        // Assert that the state is now as expected
         assertEquals(0, page0.getNumberOfPhotosInPage());
         assertEquals(1, page1.getNumberOfPhotosInPage());
     }
